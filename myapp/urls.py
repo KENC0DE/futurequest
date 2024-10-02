14b5c8a771d134138582f4cd774730750f807d0a
KENC0DE <kenansameseret@gmail.com>
2024-10-02 16:08:43 +0300

    fix-endpoint

--- myapp/urls.py
+++ myapp/urls.py
@@ -15,7 +15,7 @@ urlpatterns = [
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('validate-token/', validate_token, name='validate_token'),
-    path('', UserDetailView.as_view(), name='user_detail'),
+    path('users', UserDetailView.as_view(), name='user_detail'),
 
     path('', include(router.urls)),
 ]
