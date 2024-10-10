dc026c9649533f95e325b9e342a0052fc071a4ab
meta-bay <bayiemeseret@gmail.com>
2024-10-10 10:00:35 +0300

    fix reload error on the profile and applicationform page

--- futurequest_front/src/PrivateRoute.js
+++ futurequest_front/src/PrivateRoute.js
@@ -3,7 +3,10 @@ import { Navigate, Outlet } from "react-router-dom";
 import { AuthContext } from "./AuthContext";
 
 const PrivateRoute = () => {
-  const { user } = useContext(AuthContext);
+  const { user, isLoading } = useContext(AuthContext);
+  if (isLoading) {
+    return <p></p>;
+  }
   return user ? <Outlet /> : <Navigate to="/login" />;
 };
 
