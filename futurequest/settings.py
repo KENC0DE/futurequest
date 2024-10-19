fb30b07301a0843fd12ebb4186ab9c1e49dbdd48
KENC0DE <kenansameseret@gmail.com>
2024-10-19 11:17:32 +0300

    fix loc

--- futurequest/settings.py
+++ futurequest/settings.py
@@ -83,14 +83,14 @@ WSGI_APPLICATION = 'futurequest.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 database_url = os.environ.get('DATABASE_URL')
-DATABASES['default'] = dj_database_url.parse(database_url)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
