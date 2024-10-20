df8d18781844d3dca1ef33a93e5574bea7d4b580
KENC0DE <kenansameseret@gmail.com>
2024-10-20 16:32:18 +0300

    rol back

--- futurequest/settings.py
+++ futurequest/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = 'futurequest.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-"""
+
+database_url = os.environ.get('DATABASE_URL')
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-"""
-database_url = os.environ.get('DATABASE_URL')
-DATABASES = dj_database_url.parse(database_url)
+DATABASES['default'] = dj_database_url.parse(database_url)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
