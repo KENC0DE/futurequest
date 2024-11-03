33c59ae24f62a991b541f473a5cf3ad9de827a83
KENC0DE <kenansameseret@gmail.com>
2024-11-03 12:26:48 +0300

    remove root url from admin

--- futurequest/urls.py
+++ futurequest/urls.py
@@ -22,7 +22,6 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('api/', include('myapp.urls')),
     path('admin/', admin.site.urls),
-    path('', admin.site.urls),
 ]
 
 if settings.SERVE_FILES:
