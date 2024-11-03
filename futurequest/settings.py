5ec77e9bdf3c9938e0a486cffce93adef0d7034e
KENC0DE <kenansameseret@gmail.com>
2024-11-03 11:54:26 +0300

    add SERVE_FILE env and root url to admin

--- futurequest/settings.py
+++ futurequest/settings.py
@@ -148,6 +148,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+SERVE_FILES = os.environ.get('SERVE_FILES', False)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- futurequest/urls.py
+++ futurequest/urls.py
@@ -20,9 +20,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('api/', include('myapp.urls')),
+    path('admin/', admin.site.urls),
+    path('', admin.site.urls),
 ]
 
-if settings.DEBUG:
+if settings.SERVE_FILES:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
